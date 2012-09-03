02f64c3d7467d03606c4ccc41c71879da48c659d
johnbfarmer <john@3r3w.org>
2012-09-02 21:24:26 -0500

    I think we're ready to pushgit add images/*

--- ajax_waiver_signed.php
+++
@@ -1,9 +0,0 @@
-<?
-	include_once("session.php");
-	foreach($_POST as $key=>$dta) $$key=$dta;
-	
-		$ret=dbUpdate($tbl, array($fld=>"$val"), $whr);
-	
-	print $ret;
-	if (!$ret) print mysql_error();
-?>
