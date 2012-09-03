ad1538d84455cadc524996a36303a1ef74b0f760
johnbfarmer <john@3r3w.org>
2012-09-02 21:25:26 -0500

    I think we're ready to pushgit add images/*

--- acceptPlayerRegistration.php
+++ acceptPlayerRegistration.php
@@ -9,6 +9,7 @@
 					array('player_uid'=>$uid, 'team_uid'=>$team_id, 'season_uid'=>$season_id), true, true);
 				header("Location:roster.php");
 			}
+			$player = new Person($uid, $team_id, $season_id);
 			print "<html>";
 			print "<head>";
 			print "<link href='tmsl.css' rel='stylesheet' type='text/css'>";
@@ -29,17 +30,23 @@
 			if (empty($arr)) {$status=0;$notes='';$bal=0;}
 			else {$status = $arr[0]['registered'];$notes = $arr[0]['notes'];$bal=$arr[0]['balance'];}
 
-			print "Registration: $player_nm  for $team_nm -- $season_nm";
-			print "<br/>This player has a balance of $".$bal.".";
-			print "<form method='post'>";
+			print "Registration: $player_nm  for <a href='roster.php?team_id=$team_id&season_id=$season_id'>$team_nm</a> -- $season_nm";
+			print "<br/><br/>This player has a balance of $".$bal.".";
+			$not1 = $player->waiverSigned ? "" : "not";
+			print "<br/>This player has $not1 signed the waiver.";
+			$not2 = $player->dobValidated ? "" : "not";
+			print "<br/>This player's date of birth has $not2 been validated.<br/>";
+			if (!$not1 && !$not2) {$register = 2; $sel2="selected='true'";}
+			else {$register = 0;$sel0="selected='true'";}
+			print "<br/><form method='post'>";
 			print "Set the balance to <input type='text' name='bal' value='0' size='4'><br/>";
-			print "Notes:<br/>";
+			print "<br/>Notes:<br/>";
 			print "<textarea name='notes' rows='4' cols='60'>$notes</textarea><br/>";
-			print "Select the desired registration status:<br/>";
+			print "<br/><br/>Select the desired registration status:<br/>";
 			print "<select name='reg_status'>";
-			print "  <option value=0>Not Registered</option>";
+			print "  <option value=0 $sel0>Not Registered</option>";
 			print "  <option value=1>Registration Submitted</option>";
-			print "  <option value=2 selected='true'>Registered</option>";
+			print "  <option value=2 $sel2>Registered</option>";
 			print "  <option value=3>Registration Cancelled</option>";
 			print "</select><br/>";
 			print "<input type='submit' value='ok' name='commit'>";
